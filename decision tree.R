b0f82cb895d0cf1e4cefafe0421430fbb4665d38
Shruthi N <73688208+Shruthi100@users.noreply.github.com>
2022-01-16 16:00:38 +0530

    Delete decision tree.R

--- decision tree.R
+++
@@ -1,9 +0,0 @@
-library(rpart)
-model2 <- rpart(Outcome ~ Pregnancies + Glucose + BMI + DiabetesPedigreeFunction, data=train,method="class")
-plot(model2, uniform=TRUE, 
-     main="Classification Tree for Diabetes")
-text(model2, use.n=TRUE, all=TRUE, cex=.8)
-
-treePred <- predict(model2, test, type = 'class')
-table(treePred, test$Outcome)
-mean(treePred==test$Outcome)
